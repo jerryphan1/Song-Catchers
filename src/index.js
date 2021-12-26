909900ab24f8eccfb5b507e52c4b356da22714dd
Jerry Phan <jerryphan1@gmail.com>
2021-12-26 13:02:35 -0800

    working accord

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOM parsed');
+  console.log('DOM');
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.collapsible').forEach((button) => {
+    button.addEventListener('click', () => {
+      let content = button.nextElementSibling;
+      button.classList.toggle('active')
+      console.log(content.style.maxHeight)
+      if (!content.style.maxHeight){
+        content.style.maxHeight = content.scrollHeight + "px";
+      } else {
+        content.style.maxHeight = 0;
+      } 
+    })
+  })
+});
+
+
