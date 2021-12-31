08a76e174685e065d912a0934da112d1d5c0efd5
Jerry Phan <jerryphan1@gmail.com>
2021-12-31 10:30:46 -0800

    dry files

--- src/index.js
+++ src/index.js
@@ -1,54 +1,12 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+import {Fetch} from './scripts/fetch.js';
+import {Util} from './scripts/util.js';
+document.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM');
+  let util = new Util();
+  util.makeGraph()
+  util.collapseLeftBar()
 });
 
-window.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.collapsible').forEach((button) => {
-    button.addEventListener('click', () => {
-      // let content = button.nextElementSibling;
-      button.classList.toggle('active')
-      // console.log(content.style.maxHeight)
-      // if (!content.style.maxHeight){
-      //   content.style.maxHeight = content.scrollHeight + "px";
-      // } else {
-      //   content.style.maxHeight = 0;
-      // } 
-    })
-  })
-});
-
-
-window.addEventListener('DOMContentLoaded', (event) => {
-  const ctx = document.getElementById('myChart').getContext('2d')
-  const labels = [
-    '2012',
-    '2013',
-    '2014',
-    '2015',
-    '2016',
-  ];
-
-  const testData = {
-    labels,
-    datasets: [
-      {
-        data: [200,211,212,213,214,215],
-        backgroundColor: 'rgb(255, 10, 11)',
-        label: 'Test Graph'
-      }
-    ]
-  }
-
-  const config = {
-    type: 'line',
-    data: testData,
-    options: {
-      responsive: true,
-    },
-  }
-
-  const myChart = new Chart(ctx,config)
-});
 
 // fetch("https://api.spotify.com/v1/search?q=blank%20space%2C%20taylor%20swift&type=track%2Cartist&market=ES&limit=1&offset=0", {
 // 	"method": "GET",
@@ -63,48 +21,73 @@ window.addEventListener('DOMContentLoaded', (event) => {
 //   .then(data => console.log(data))
 //   .catch(err => console.log(err))
 
-window.addEventListener('DOMContentLoaded', (event) => {
-const APIController = (function() {
-  console.log('testi')
-  const clientId = '';
-  const clientSecret = '';
+var client_id = '661979470da84b9ead5db04fcede6ada';
+var client_secret = '9a759d370dc34783b8e30e0375999dca';
 
-  const _getToken = async () => {
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
 
-    const result = await fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type' : 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64'))
-      },
-      body:  'grant_type=client_credentials'
-    });
-
-    const data = await result.json();
-    return data.access_token;
+request.post(authOptions, function(error, response, body) {
+  console.log('test')
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
   }
-
-  const _getSearch = async (token) => {
-
-    const result = await fetch("https://api.spotify.com/v1/search?q=last%20cup%20of%20coffee%2C%20valkyrae&type=track%2Cartist&market=ES&limit=1&offset=0", {
-      method: 'GET',
-      headers: { 'Authorization' : 'Bearer ' + token}
-    });
-
-    const data = await result.json();
-    // return data.artists.items;
-    console.log(data)
-  }
-
-  return {
-    getToken() {
-      return _getToken();
-    },
-
-    getSearch() {
-      return _getSearch(token)
-    }
-  }
-
-})()
 });
+
+
+
+// window.addEventListener('DOMContentLoaded', (event) => {
+// const APIController = (function() {
+//   console.log('testi')
+//   const clientId = '661979470da84b9ead5db04fcede6ada';
+//   const clientSecret = '9a759d370dc34783b8e30e0375999dca';
+
+//   const _getToken = async () => {
+
+//     const result = await fetch('https://accounts.spotify.com/api/token', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type' : 'application/x-www-form-urlencoded',
+//         'Authorization': 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64'))
+//       },
+//       body:  'grant_type=client_credentials'
+//     });
+
+//     const data = await result.json();
+//     console.log('hi')
+//     console.log(data.access_token)
+//     return data.access_token;
+//   }
+
+//   const _getSearch = async (token) => {
+
+//     const result = await fetch("https://api.spotify.com/v1/search?q=last%20cup%20of%20coffee%2C%20valkyrae&type=track%2Cartist&market=ES&limit=1&offset=0", {
+//       method: 'GET',
+//       headers: { 'Authorization' : 'Bearer ' + token}
+//     });
+
+//     const data = await result.json();
+//     // return data.artists.items;
+//     console.log(data)
+//   }
+
+//   return {
+//     getToken() {
+//       return _getToken();
+//     },
+
+//     getSearch() {
+//       return _getSearch(token)
+//     }
+//   }
+
+// })()
+// });
---
+++ src/scripts/util.js
@@ -0,0 +1,48 @@
+export class Util {
+  makeGraph() {
+    const ctx = document.getElementById('myChart').getContext('2d')
+    const labels = [
+      '2012',
+      '2013',
+      '2014',
+      '2015',
+      '2016',
+    ];
+  
+    const testData = {
+      labels,
+      datasets: [
+        {
+          data: [200,211,212,213,214,215],
+          backgroundColor: 'rgb(255, 10, 11)',
+          label: 'Test Graph'
+        }
+      ]
+    }
+  
+    const config = {
+      type: 'line',
+      data: testData,
+      options: {
+        responsive: true,
+      },
+    }
+  
+    const myChart = new Chart(ctx,config)
+  }
+
+  collapseLeftBar() {
+    document.querySelectorAll('.collapsible').forEach((button) => {
+      button.addEventListener('click', () => {
+        // let content = button.nextElementSibling;
+        button.classList.toggle('active')
+        // console.log(content.style.maxHeight)
+        // if (!content.style.maxHeight){
+        //   content.style.maxHeight = content.scrollHeight + "px";
+        // } else {
+        //   content.style.maxHeight = 0;
+        // } 
+      })
+    })
+  }
+}
